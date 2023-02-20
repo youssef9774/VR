54ea33f3661f2ba25c0b0e975b34c9cf202b1d97
youssef9774 <youssefsassi9774@gmail.com>
2023-02-20 17:35:35 +0100

    first commit

---
+++ Assets/Oculus/LipSync/Scripts/ChangeAlpha.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAlpha : MonoBehaviour
+{
+    public Slider mainSlider;
+    public GameObject currentGameObject;
+    private Material currentMat;
+    private float alpha;
+
+    void Start()
+    {
+            currentGameObject = gameObject;
+            currentMat = currentGameObject.GetComponent<Renderer>().material;
+            mainSlider.value = currentMat.color.a;    
+    }
+
+    // Update is called once per frame
+    public void UpdateAlpha()
+    {
+        Color oldColor = currentMat.color;
+        alpha = mainSlider.value;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alpha);
+        currentMat.SetColor("_Color", newColor);
+    }
+}
